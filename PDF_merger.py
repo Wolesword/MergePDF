77a2d5164f7b6a9ef360798fb70d679797db6d3e
DataScienceWiz <wolesword@gmail.com>
2020-12-14 12:29:41 -0500

    Added streamlit components

--- PDF_merger.py
+++ PDF_merger.py
@@ -9,23 +9,3 @@ for pdf in pdfs:
 
 merger.write("OneFile.pdf")
 merger.close()
-
-'''from PyPDF2 import PdfFileMerger
-import webbrowser
-import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-def list_files(directory, extension):
-    return (f for f in os.listdir(directory) if f.endswith('.' + extension))
-
-pdfs = list_files(dir_path, "pdf")
-
-merger = PdfFileMerger(strict=False)
-
-for pdf in pdfs:
-    merger.append(open(pdf, 'rb'))
-
-with open('results.pdf', 'wb') as fout:
-    merger.write(fout)
-
-webbrowser.open_new('file://'+ dir_path + '/result.pdf')'''
