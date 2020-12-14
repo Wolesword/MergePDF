ff392df5977c984ff90fb42dbf372ef3672d6d40
DataScienceWiz <wolesword@gmail.com>
2020-12-14 12:31:21 -0500

    Added streamlit components

---
+++ Py2PDF.py
@@ -0,0 +1,20 @@
+''' from PyPDF2 import PdfFileMerger '''
+
+import webbrowser
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def list_files(directory, extension):
+    return (f for f in os.listdir(directory) if f.endswith('.' + extension))
+
+pdfs = list_files(dir_path, "pdf")
+
+merger = PdfFileMerger(strict=False)
+
+for pdf in pdfs:
+    merger.append(open(pdf, 'rb'))
+
+with open('results.pdf', 'wb') as fout:
+    merger.write(fout)
+
+webbrowser.open_new('file://'+ dir_path + '/result.pdf')
